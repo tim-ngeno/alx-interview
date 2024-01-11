eb3720ff1546d57d4de49eff95c92f670907ff27
Tim Ngeno <timngeno424@gmail.com>
2024-01-11 16:42:50 +0300

    fix lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,25 +4,52 @@ Solving the lockboxes algorithm challenge
 """
 
 
+# def canUnlockAll(boxes):
+#     """
+#     Determines if all boxes in a list can be opened
+
+#     Args:
+#         boxes (list): a multidimensional list of boxes
+#     """
+#     visited_boxes = set()
+
+#     def explore_boxes(box_index):
+#         """
+#         Traverses all the boxes by index
+#         """
+#         if box_index in visited_boxes:
+#             return
+#         visited_boxes.add(box_index)
+
+#         for key in boxes[box_index]:
+#             explore_boxes(key)
+
+#     explore_boxes(0)
+#     return len(visited_boxes) == len(boxes)
+
+
 def canUnlockAll(boxes):
     """
-    Determines if all boxes in a list can be opened
+    Determines if all the boxes can be opened.
 
-    Args:
-        boxes (list): a multidimensional list of boxes
+    Parameters:
+    - boxes (List[List[int]]): A list of lists where each inner list represents a box and contains keys to other boxes.
+
+    Returns:
+    - bool: True if all boxes can be opened, False otherwise.
     """
-    visited_boxes = set()
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True  # The first box is always unlocked.
 
-    def explore_boxes(box_index):
-        """
-        Traverses all the boxes by index
-        """
-        if box_index in visited_boxes:
-            return
-        visited_boxes.add(box_index)
+    stack = [0]  # Start with the first box.
 
-        for key in boxes[box_index]:
-            explore_boxes(key)
+    while stack:
+        current_box = stack.pop()
 
-    explore_boxes(0)
-    return len(visited_boxes) == len(boxes)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
