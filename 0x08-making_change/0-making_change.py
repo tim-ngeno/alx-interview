05c3a730c2d2470365e89f1577e46b3a897cff5d
Tim Ngeno <timngeno424@gmail.com>
2024-02-29 16:04:12 +0300

    determine the fewest number of coins needed

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" The coin change problem """
+
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given amount
+    """
+    if total <= 0:
+        return 0
+
+    # array to store minimum number of coins needed
+    dp = [float('inf')] * (total + 1)
+    # base case
+    dp[0] = 0
+
+    # Iterate through the various denominations
+    for coin in coins:
+        for i in range(coin, total + 1):
+            # Update minimum number of coins for each value
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
