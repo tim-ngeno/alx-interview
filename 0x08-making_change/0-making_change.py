c1ea03ca362778c2e42c04d313babd71d8242f42
Tim Ngeno <timngeno424@gmail.com>
2024-03-02 10:14:19 +0300

    use greedy approach

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,21 +6,37 @@ def makeChange(coins, total):
     """
     Determines the fewest number of coins needed to meet a given amount
     """
-    # Sort the coins in reverse order
-    coins.sort(reverse=True)
+    # # Sort the coins in reverse order
+    # coins.sort(reverse=True)
+
+    # if total <= 0:
+    #     return 0
+
+    # # array to store minimum number of coins needed
+    # dp = [float('inf')] * (total + 1)
+    # # base case
+    # dp[0] = 0
+
+    # # Iterate through the various denominations
+    # for coin in coins:
+    #     for i in range(coin, total + 1):
+    #         # Update minimum number of coins for each value
+    #         dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    # return dp[total] if dp[total] != float('inf') else -1
 
     if total <= 0:
         return 0
 
-    # array to store minimum number of coins needed
-    dp = [float('inf')] * (total + 1)
-    # base case
-    dp[0] = 0
+    coins.sort(reversed=True)
+    i = 0
+    count = 0
 
-    # Iterate through the various denominations
-    for coin in coins:
-        for i in range(coin, total + 1):
-            # Update minimum number of coins for each value
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+    while i < len(coins):
+        count += total // coins[i]
+        total = total % coins[i]
+        i += 1
+        if total == 0:
+            return count
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return -1
